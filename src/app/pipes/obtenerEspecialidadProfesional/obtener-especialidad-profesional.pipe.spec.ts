6fb760bfe14d262b389208baf3a63db82730bad9
barrioscrespomatias <matisfd@gmail.com>
2023-06-24 18:23:06 -0300

    pipes filtros

---
+++ src/app/pipes/obtenerEspecialidadProfesional/obtener-especialidad-profesional.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ObtenerEspecialidadProfesionalPipe } from './obtener-especialidad-profesional.pipe';
+
+describe('ObtenerEspecialidadProfesionalPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ObtenerEspecialidadProfesionalPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/obtenerProfesionalPaciente/obtener-profesional-paciente.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ObtenerProfesionalPacientePipe } from './obtener-profesional-paciente.pipe';
+
+describe('ObtenerProfesionalPacientePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ObtenerProfesionalPacientePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
