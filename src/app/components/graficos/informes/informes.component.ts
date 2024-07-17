d3e80d5699310737c9bee79a890e86341d0c50b1
barrioscrespomatias <matisfd@gmail.com>
2024-07-17 11:40:54 -0300

    informes

--- src/app/components/graficos/informes/informes.component.ts
+++ src/app/components/graficos/informes/informes.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 import { Turno } from 'src/app/interfaces/turno';
 import { UsuarioService } from 'src/app/servicios/entidades/usuario/usuario.service';
 import { TurnoV2Service } from 'src/app/servicios/v2/turno-v2.service';
@@ -31,7 +31,8 @@ export class InformesComponent implements OnInit{
   
   constructor(private usuarioService: UsuarioV2Service,
               private turnoService: TurnoV2Service,
-              private firebaseService:FirebaseAuthService
+              private firebaseService:FirebaseAuthService,
+              private changeDetector: ChangeDetectorRef 
   ){
 
   }
@@ -42,15 +43,47 @@ export class InformesComponent implements OnInit{
   turnosPorPaciente: Turno[] = [];
   datePropertyFalse = false;
   isLogged: boolean = false;
+  turnosRealizados: Turno[] = [];
+
+
 
   // Turnos por paciente
   data1: number[] = [];
   chartsLabels1: Array<string> = [];
   title1 = 'Turnos por paciente';
-  type1 = 'pie';
+  type1 = 'line';
   chartSelector1 = '.chart-1';
 
+  // Cantidad de pacientes por especialidad
+  data2: number[] = [];
+  chartsLabels2: Array<string> = [];
+  title2 = 'Cantidad de pacientes por especialidad';
+  type2 = 'bar';
+  chartSelector2 = '.chart-2';
+
+  // Cantidad de pacientes por especialidad
+  data3: number[] = [];
+  chartsLabels3: Array<string> = [];
+  title3 = 'Cantidad de medicos por especialidad';
+  type3 = 'line';
+  chartSelector3 = '.chart-3';
+
+    // Cantidad de pacientes por especialidad
+  data4: number[] = [];
+  chartsLabels4: Array<string> = [];
+  title4 = 'Cantidad de visitas';
+  type4 = 'bar';
+  chartSelector4 = '.chart-4';
+
   //#endregion
+
+  groupArray(data: any[], size: number): any[][] {
+    let newArray = [];
+    for (let i = 0; i < data.length; i += size) {
+      newArray.push(data.slice(i, i + size));
+    }
+    return newArray;
+  }
 
   receiveMessage(paciente:any) {
     this.messageInput = paciente;
@@ -66,15 +99,62 @@ export class InformesComponent implements OnInit{
   async ngOnInit(): Promise<void> {
     await this.checkLoggedIn();
 
+    this.turnoService.traerTurnoPorEstado('Realizado').subscribe(async (t) => {
+      this.turnosRealizados = t as Turno[];
+
+      if(this.turnosRealizados){
+        await this.calcularCantidadVisitas();
+        await this.calcularCantidadPacientesPorEspecialidad()
+        await this.calcularCantidadMedicosPorEspecialidad()
+      }
+    });
   }
 
   async checkLoggedIn() {
     this.isLogged = await this.firebaseService.isLoggedIn();
   }
 
+  async calcularCantidadVisitas(){
+
+    this.data4.push(this.turnosRealizados.length);
+    this.chartsLabels4.push('Cantidad de visitas')
+
+    this.changeDetector.detectChanges();
+  };
+
+  async calcularCantidadPacientesPorEspecialidad() {
+    var turnosPorEspecialidad = this.AgruparTurnosPorEspecialidad(this.turnosRealizados);
+    this.GenerarDatosPacientesPorEspecialidad(turnosPorEspecialidad,  'chart-2')
+  }
+
+  async calcularCantidadMedicosPorEspecialidad() {
+    var turnosPorEspecialidad = this.AgruparTurnosPorEspecialidad(this.turnosRealizados);
+    this.GenerarDatosMedicosPorEspecialidad(turnosPorEspecialidad,  'chart-3')
+  }
+
   calcularTurnosPorPaciente() {
     var turnosPorEstado = this.AgruparTurnosPorEstado(this.turnosPorPaciente);
     this.GenerarDatosTurnosPacientePorEstado(turnosPorEstado,  'chart-1')
+  }
+
+  AgruparTurnosPorEspecialidad(turnos: Turno[]): Map<string, Turno[]> {
+    const turnosAgrupados = new Map<string, Turno[]>();
+
+    for (const turno of turnos) {
+      // Utilizar la especialidad como clave
+      const claveEspecialidad = turno.especialidad;
+
+      // Si la clave aún no existe en el mapa, agregar un nuevo array y el turno
+      if (claveEspecialidad !== undefined) {
+        if (!turnosAgrupados.has(claveEspecialidad)) {
+          turnosAgrupados.set(claveEspecialidad, [turno]);
+        } else {
+          // Si la clave ya existe, agregar el turno al array existente
+          turnosAgrupados.get(claveEspecialidad)!.push(turno);
+        }
+      }
+    }
+    return turnosAgrupados;
   }
 
   AgruparTurnosPorEstado(turnos: Turno[]): Map<string, Turno[]> {
@@ -95,6 +175,62 @@ export class InformesComponent implements OnInit{
       }
     }
     return turnosAgrupados;
+  }
+
+  GenerarDatosPacientesPorEspecialidad(turnosAgrupados: Map<string, Turno[]>, chart: string) {
+    const data: number[] = [];
+    const chartsLabels: string[] = [];
+
+    // Iterar sobre el mapa y llenar los chartslabels
+    for (const [especialidad, turnos] of turnosAgrupados) {
+      chartsLabels.push(especialidad);
+    }
+
+    const pacientesUnicos = new Set<string>();
+    // Iterar sobre el mapa y llenar los data
+    for (const [especialidad, turnos] of turnosAgrupados) {
+      for (let turno of turnos){
+        if(turno.paciente)
+          pacientesUnicos.add(turno.paciente);
+      }
+      data.push(pacientesUnicos.size);
+    }
+  
+  // data.push(turnos.length);
+    switch (chart) {
+      case 'chart-2':
+        this.data2 = data;
+        this.chartsLabels2 = chartsLabels;
+        break;
+    }
+  }
+
+  GenerarDatosMedicosPorEspecialidad(turnosAgrupados: Map<string, Turno[]>, chart: string) {
+    const data: number[] = [];
+    const chartsLabels: string[] = [];
+
+    // Iterar sobre el mapa y llenar los chartslabels
+    for (const [especialidad, turnos] of turnosAgrupados) {
+      chartsLabels.push(especialidad);
+    }
+
+    const profesionalesUnicos = new Set<string>();
+    // Iterar sobre el mapa y llenar los data
+    for (const [especialidad, turnos] of turnosAgrupados) {
+      for (let turno of turnos){
+        if(turno.profesional)
+          profesionalesUnicos.add(turno.profesional);
+      }
+      data.push(profesionalesUnicos.size);
+    }
+  
+  // data.push(turnos.length);
+    switch (chart) {
+      case 'chart-3':
+        this.data3 = data;
+        this.chartsLabels3 = chartsLabels;
+        break;
+    }
   }
 
   GenerarDatosTurnosPacientePorEstado(turnosAgrupados: Map<string, Turno[]>, chart: string) {
