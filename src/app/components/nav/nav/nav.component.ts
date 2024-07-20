eab326cca68e05fa904ab3705c2117bb5627aab7
barrioscrespomatias <matisfd@gmail.com>
2024-07-20 13:06:33 -0300

    estilos navbar

--- src/app/components/nav/nav/nav.component.ts
+++ src/app/components/nav/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { FirebaseAuthService } from '../../../services/angularFire/angular-fire.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,17 +8,31 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
   constructor(public firebaseService: FirebaseAuthService) {
     this.checkLoggedIn();
   }
   public isLogged: boolean = false;
   public userName: string = this.firebaseService.userName;
+
+  idioma:string = 'espanol';
+  idioma_src:string = '../../../../assets/img/idioma/'+ this.idioma +'.png';
+
   async checkLoggedIn() {
     this.isLogged = await this.firebaseService.isLoggedIn();
   }
 
+  ngOnInit(): void {
+    console.log(this.isLogged)
+  }
+
   SignOut() {
     this.firebaseService.SignOut();
+  }
+
+  cambiarIdioma(idioma:string) {
+    
+    this.idioma = idioma;
+    this.idioma_src = '../../../../assets/img/idioma/'+ this.idioma +'.png';
   }
 }
